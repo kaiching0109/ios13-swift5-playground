761dc8a6e186c79cdb955b3817c871521df673ae
kaiching0109 <aaronsuen0109@gmail.com>
2020-05-22 10:02:34 +0900

    COMPLETED: egg timer

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var timer = Timer()
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    let time = [
+        "soft": 3,
+        "medium": 420,
+        "hard": 720
+    ]
+    var totalTime = 0
+    
+    var secondsRemaining = 0
 
-
+    @objc func updateTime() {
+        if(secondsRemaining > 0) {
+            secondsRemaining -= 1
+            progressView.progress = Float(secondsRemaining) / Float(totalTime)
+        } else {
+            progressView.progress = 0
+            timerLabel.text = "DONE"
+            timer.invalidate()
+        }
+    }
+    
+    @IBAction func eggKeyPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle
+        progressView.progress = 1.0
+        let type = hardness!.components(separatedBy: " ")[0]
+        
+        timerLabel.text = type
+        
+        let key = type.lowercased()
+        
+        secondsRemaining = time[key]!
+        totalTime = secondsRemaining
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+    }
+    
 }
